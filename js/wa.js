92e036f03e43140826f84e03d773e751daa5ec9c
risqoh <risqoh5758@gmail.com>
2024-09-19 15:45:06 +0700

    produk update

--- js/wa.js
+++ js/wa.js
@@ -8,3 +8,124 @@ boxes.forEach(function (box) {
     );
   });
 });
+
+//WA RAK ROKOK MODEL BARU
+var boxes = document.querySelectorAll(".rak-rokok-model-baru");
+boxes.forEach(function (box) {
+  box.addEventListener("click", function () {
+    window.open(
+      "https://api.whatsapp.com/send?phone=6285198995587&text=halo%20kak%2C%20saya%20ingin%20membeli%20produk%20rak%20rokok%20model%20baru",
+      "_blank"
+    );
+  });
+});
+
+//WA RAK SNACK/KACANG DOUBLE
+var boxes = document.querySelectorAll(".rak-snack-kacang-double");
+boxes.forEach(function (box) {
+  box.addEventListener("click", function () {
+    window.open(
+      "https://api.whatsapp.com/send?phone=6285198995587&text=halo%20kak%2C%20saya%20ingin%20membeli%20produk%20rak%20snack/kacang%20double",
+      "_blank"
+    );
+  });
+});
+
+//WA RAK SNACK/KACANG SINGLE
+var boxes = document.querySelectorAll(".rak-snack-kacang-single");
+boxes.forEach(function (box) {
+  box.addEventListener("click", function () {
+    window.open(
+      "https://api.whatsapp.com/send?phone=6285198995587&text=halo%20kak%2C%20saya%20ingin%20membeli%20produk%20rak%20snack/kacang%20single",
+      "_blank"
+    );
+  });
+});
+
+//WA RAK MARKET SINGLE
+var boxes = document.querySelectorAll(".rak-market-single");
+boxes.forEach(function (box) {
+  box.addEventListener("click", function () {
+    window.open(
+      "https://api.whatsapp.com/send?phone=6285198995587&text=halo%20kak%2C%20saya%20ingin%20membeli%20produk%20rak%20market%20single",
+      "_blank"
+    );
+  });
+});
+
+//WA RAK MARKET DOUBLE
+var boxes = document.querySelectorAll(".rak-market-double");
+boxes.forEach(function (box) {
+  box.addEventListener("click", function () {
+    window.open(
+      "https://api.whatsapp.com/send?phone=6285198995587&text=halo%20kak%2C%20saya%20ingin%20membeli%20produk%20rak%20market%20double",
+      "_blank"
+    );
+  });
+});
+
+//WA RAK EKONOMIS SUDUT
+var boxes = document.querySelectorAll(".rak-ekonomis-sudut");
+boxes.forEach(function (box) {
+  box.addEventListener("click", function () {
+    window.open(
+      "https://api.whatsapp.com/send?phone=6285198995587&text=halo%20kak%2C%20saya%20ingin%20membeli%20produk%20rak%20ekonomis%20sudut",
+      "_blank"
+    );
+  });
+});
+
+//WA RAK EKONOMIS DOUBLE
+var boxes = document.querySelectorAll(".rak-ekonomis-double");
+boxes.forEach(function (box) {
+  box.addEventListener("click", function () {
+    window.open(
+      "https://api.whatsapp.com/send?phone=6285198995587&text=halo%20kak%2C%20saya%20ingin%20membeli%20produk%20rak%20ekonomis%20double",
+      "_blank"
+    );
+  });
+});
+
+//WA RAK EKONOMIS SINGLE
+var boxes = document.querySelectorAll(".rak-ekonomis-single");
+boxes.forEach(function (box) {
+  box.addEventListener("click", function () {
+    window.open(
+      "https://api.whatsapp.com/send?phone=6285198995587&text=halo%20kak%2C%20saya%20ingin%20membeli%20produk%20rak%20ekonomis%20single",
+      "_blank"
+    );
+  });
+});
+
+//WA MEJA KASIR
+var boxes = document.querySelectorAll(".meja-kasir");
+boxes.forEach(function (box) {
+  box.addEventListener("click", function () {
+    window.open(
+      "https://api.whatsapp.com/send?phone=6285198995587&text=halo%20kak%2C%20saya%20ingin%20membeli%20produk%20meja%20kasir",
+      "_blank"
+    );
+  });
+});
+
+//WA PAKET MESIN KASIR ANDROID
+var boxes = document.querySelectorAll(".paket-mesin-kasir-android");
+boxes.forEach(function (box) {
+  box.addEventListener("click", function () {
+    window.open(
+      "https://api.whatsapp.com/send?phone=6285198995587&text=halo%20kak%2C%20saya%20ingin%20membeli%20produk%20paket%20mesin%20kasir%20android",
+      "_blank"
+    );
+  });
+});
+
+//WA PAKET KOMPUTER KASIR
+var boxes = document.querySelectorAll(".paket-komputer-kasir");
+boxes.forEach(function (box) {
+  box.addEventListener("click", function () {
+    window.open(
+      "https://api.whatsapp.com/send?phone=6285198995587&text=halo%20kak%2C%20saya%20ingin%20membeli%20produk%20paket%20komputer%20kasir",
+      "_blank"
+    );
+  });
+});
